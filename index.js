21a71d015b281538f9d4a89163a00b7993162d56
MartinZazpe <martinzazpe1@gmail.com>
2022-11-25 23:42:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const product = require('./api/product')
+
+
+app.listen(port, () => {
+    console.log(`server is running in port ${port}`)
+})
+
+app.use('/api/product', product)
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
