0d5f8aeb001645c7bda062c81a1dce904f3eed5c
MartinZazpe <martinzazpe1@gmail.com>
2022-11-26 00:17:36 +0100

    routes

--- api/product.js
+++ api/product.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
 
     // try {
     //     res.json({
@@ -14,8 +14,7 @@ router.get("/", async (req, res) => {
     //     console.log(error)
     //     return res.status(500).send("server error")
     // }
-    res.render('./index')
-
+    res.render('index')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
-
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+
+var path = require('path')
+app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, 'public')))
 
 const product = require('./api/product')
 
@@ -11,6 +13,7 @@ const product = require('./api/product')
 app.listen(port, () => {
     console.log(`server is running in port ${port}`)
 })
+
 
 app.use('/', product)
 
