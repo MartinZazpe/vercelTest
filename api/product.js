1a236083cb8a9227c3af0ce3970916ddd35fb901
MartinZazpe <martinzazpe1@gmail.com>
2022-11-25 23:57:11 +0100

    testing routes

--- api/product.js
+++ api/product.js
@@ -4,17 +4,18 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
 
-    try {
-        // res.json({
-        //     status: 200,
-        //     message: "get data has succesfully"
-        // })
-        res.render('index')
-    }
-    catch (error) {
-        console.log(error)
-        return res.status(500).send("server error")
-    }
+    // try {
+    //     res.json({
+    //         status: 200,
+    //         message: "get data has succesfully"
+    //     })
+    // }
+    // catch (error) {
+    //     console.log(error)
+    //     return res.status(500).send("server error")
+    // }
+    res.render('index')
+
 })
 
 module.exports = router
